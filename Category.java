82047318b2886b5a114bd46296e14bb63d9fdb2e
Pejman Adib Ghodsi <63265453+Pejman-Adib-Ghodsi@users.noreply.github.com>
2026-04-29 02:22:29 +0200

    refactor: use NotBlank for category name validation

--- Category.java
+++ Category.java
@@ -1,11 +1,10 @@
 package be.computershop.computershopapi.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import javax.validation.constraints.NotNull;
 import lombok.*;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
 import java.util.Set;
 
 @Getter
@@ -21,8 +20,7 @@ public class Category {
     private Long id;
 
     @Column(unique = true)
-    @NotNull
-    @NotEmpty
+    @NotBlank
     private String name;
 
     @OneToMany(mappedBy = "category")
